98f0ac39826b81c3d1fa102e802f43f005baab8a
Evan Cunningham <ecunningham@sd38.bc.ca>
2021-08-10 15:08:55 -0700

    updating tsc.php

--- web/tsc.php
+++ web/tsc.php
@@ -1,3 +1,18 @@
+<!DOCTYPE html>
 <html>
-This is a test landing page.
+<head>
+<title>Page Title</title>
+<style>
+body {
+  font-family: Helvetica, Verdana;
+}
+</style>
+</head>
+<body>
+
+<img src="https://sd38.bc.ca/sites/default/files/LogoHeader_0.png" width="250" height="auto">
+<h1>TSC Landing Page</h1>
+<p>Content to go here...</p>
+
+</body>
 </html>
