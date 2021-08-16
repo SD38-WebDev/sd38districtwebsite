2672597b9604aa4c09f565ea4451c7b02cd8fc7d
Evan Cunningham <ecunningham@sd38.bc.ca>
2021-08-16 10:16:16 -0700

    updated TSC.php file

--- web/tsc.php
+++ web/tsc.php
@@ -1,18 +1,20 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Page Title</title>
+<title>SD38 – Password Change Error</title>
 <style>
 body {
   font-family: Helvetica, Verdana;
+  font-size: 1rem;
 }
 </style>
 </head>
 <body>
 
 <img src="https://sd38.bc.ca/sites/default/files/LogoHeader_0.png" width="250" height="auto">
-<h1>TSC Landing Page</h1>
-<p>Content to go here...</p>
+<h1>Password Change Error</h1>
+<p>This function is currently disabled, please ensure your device is connected to SD38 network and try again.</p>
+<p>Please contact the <a href="https://helpdesk.sd38.bc.ca" target="_blank">Tech Help Desk</a> for further assistance.</p>
 
 </body>
 </html>
